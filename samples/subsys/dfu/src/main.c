a003b1ceb32cf0829b7b6b54c18ba0b0ec4026f6
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2017-07-17 13:57:05 +0200

    sample for compile dfu subsys

---
+++ samples/subsys/dfu/src/main.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 Linaro Limited
+ *               2016 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <flash.h>
+#include <device.h>
+#include <stdio.h>
+
+#include "boot/mcuboot.h"
+
+void main(void)
+{
+}
