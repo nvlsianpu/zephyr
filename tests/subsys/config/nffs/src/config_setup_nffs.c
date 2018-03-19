525cff1d7e4cd7813a83fbdd4f17d40106a2022c
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2018-03-19 15:27:24 +0100

    test: subsbys: config: try to use fatfs
    Didn't work so far.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- tests/subsys/config/nffs/src/config_setup_nffs.c
+++ tests/subsys/config/nffs/src/config_setup_nffs.c
@@ -9,6 +9,7 @@
 #include <device.h>
 #include <fs.h>
 
+#if 0
 /* NFFS work area strcut */
 static struct nffs_flash_desc flash_desc;
 
@@ -18,12 +19,22 @@ static struct fs_mount_t nffs_mnt = {
 	.mnt_point = TEST_FS_MPTR,
 	.fs_data = &flash_desc,
 };
+#endif
+/* FatFs work area */
+static FATFS fat_fs;
+
+/* mounting info */
+static struct fs_mount_t fatfs_mnt = {
+	.type = FS_FATFS,
+	.mnt_point = TEST_FS_MPTR,
+	.fs_data = &fat_fs
+};
 
 void config_setup_nffs(void)
 {
 	struct device *flash_dev;
 	int rc;
-
+#if 0
 	flash_dev = device_get_binding(CONFIG_FS_NFFS_FLASH_DEV_NAME);
 	zassert_not_null(flash_dev, "Can't bind to the flash device\n");
 
@@ -31,6 +42,14 @@ void config_setup_nffs(void)
 	nffs_mnt.storage_dev = flash_dev;
 
 	rc = fs_mount(&nffs_mnt);
+	zassert_true(rc == 0, "mounting nffs [%d]\n", rc);
+#endif
+	flash_dev = device_get_binding(FLASH_DEV_NAME);
+	zassert_not_null(flash_dev, "Can't bind to the flash device\n");
+
+	/* set backend storage dev */
+	fatfs_mnt.storage_dev = flash_dev;
+	rc = fs_mount(&fatfs_mnt);
 	zassert_true(rc == 0, "mounting nffs [%d]\n", rc);
 
 	rc = fs_unlink(TEST_CONFIG_DIR);
