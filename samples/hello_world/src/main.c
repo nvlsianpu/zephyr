66bd5bcaae3604fe29ce77697a65c0b38f50ae1f
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2018-06-07 15:58:32 +0200

    samples: hello_word: add fatfs mount operation
    hello word was modified in order to show fat-fs mounting
    on nrf52840_pca10056.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -7,7 +7,34 @@
 #include <zephyr.h>
 #include <misc/printk.h>
 
+#include <fs.h>
+#include <ff.h>
+
+/* FatFs work area */
+static FATFS fat_fs;
+#define FATFS_MNTP	"/NAND:"
+
+/* mounting info */
+static struct fs_mount_t fatfs_mnt = {
+	.type = FS_FATFS,
+	.mnt_point = FATFS_MNTP,
+	.fs_data = &fat_fs,
+};
+
+static void test_mount(void)
+{
+	int res;
+
+	res = fs_mount(&fatfs_mnt);
+	if (res < 0) {
+		printk("Error mounting fs [%d]\n", res);
+	} else {
+		printk("Success mounting fs [%d]\n", res);
+	}
+}
+
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_ARCH);
+	test_mount();
 }
