1b3f3b9e6d901d5761d816dd7b504b1161aa0057
Findlay Feng <i@fengch.me>
2018-07-21 08:59:45 +0800

    dts: flash_map: Automatically generate of the list of flash area
    This Patch add functionality for automatic generation of the flash map
    using DTS description. Automatic generation allows to replace
    C-hardcoded flash_map.
    
    Signed-off-by: Findlay Feng <i@fengch.me>

--- subsys/storage/flash_map/flash_map_default.c
+++ subsys/storage/flash_map/flash_map_default.c
@@ -8,54 +8,18 @@
 #include <zephyr.h>
 #include <flash_map.h>
 
-#if defined(DT_FLASH_DEV_NAME)
-#define FA_DEV_ID SOC_FLASH_0_ID
-#elif defined(CONFIG_SPI_FLASH_W25QXXDV) || defined(DT_JEDEC_SPI_NOR_0_LABEL)
-#define FA_DEV_ID SPI_FLASH_0_ID
-#endif
+#define FLASH_AREA_OFFSET(__name)                                              \
+	UTIL_CAT(FLASH_AREA_, UTIL_CAT(__name, _OFFSET))
+#define FLASH_AREA_SIZE(__name) UTIL_CAT(FLASH_AREA_, UTIL_CAT(__name, _SIZE))
+
+#define FLASH_AREA_FOO(i, _)                                                   \
+	{.fa_id = i,                                                           \
+	 .fa_device_id = FLASH_AREA_##i##_DEV_ID,                              \
+	 .fa_off = FLASH_AREA_OFFSET(FLASH_AREA_##i),                          \
+	 .fa_size = FLASH_AREA_SIZE(FLASH_AREA_##i)},
 
 const struct flash_area default_flash_map[] = {
-	/* FLASH_AREA_BOOTLOADER */
-	{
-		.fa_id = 0,
-		.fa_device_id = FA_DEV_ID,
-		.fa_off = FLASH_AREA_MCUBOOT_OFFSET,
-		.fa_size = FLASH_AREA_MCUBOOT_SIZE,
-	},
-
-	/* FLASH_AREA_IMAGE_0 */
-	{
-		.fa_id = 1,
-		.fa_device_id = FA_DEV_ID,
-		.fa_off = FLASH_AREA_IMAGE_0_OFFSET,
-		.fa_size = FLASH_AREA_IMAGE_0_SIZE,
-	},
-
-	/* FLASH_AREA_IMAGE_1 */
-	{
-		.fa_id = 2,
-		.fa_device_id = FA_DEV_ID,
-		.fa_off = FLASH_AREA_IMAGE_1_OFFSET,
-		.fa_size = FLASH_AREA_IMAGE_1_SIZE,
-	},
-
-	/* FLASH_AREA_IMAGE_SCRATCH */
-	{
-		.fa_id = 3,
-		.fa_device_id = FA_DEV_ID,
-		.fa_off = FLASH_AREA_IMAGE_SCRATCH_OFFSET,
-		.fa_size = FLASH_AREA_IMAGE_SCRATCH_SIZE,
-	},
-
-#ifdef CONFIG_FS_FLASH_STORAGE_PARTITION
-	/* FLASH_AREA_STORAGE */
-	{
-		.fa_id = 4,
-		.fa_device_id = FA_DEV_ID,
-		.fa_off = FLASH_AREA_STORAGE_OFFSET,
-		.fa_size = FLASH_AREA_STORAGE_SIZE,
-	},
-#endif
+	UTIL_LISTIFY(FLASH_AREA_NUM, FLASH_AREA_FOO, ~)
 };
 
 const int flash_map_entries = ARRAY_SIZE(default_flash_map);
